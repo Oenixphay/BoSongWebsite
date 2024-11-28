809e732057315d1172adbcc60ec4276b10d3c305
Phoenix <phoenix.thorwall@gmail.com>
2024-11-28 14:47:17 -0500

    hello

---
+++ script.js
@@ -0,0 +1,8 @@
+Reveal = function(text){
+    var head = document.getElementById(text);
+    if(head.style.display == 'none'){
+       head.style.display = 'block';
+    } else {
+      head.style.display = 'none';
+    }
+ }
