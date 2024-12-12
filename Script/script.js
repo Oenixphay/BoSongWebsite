2b9ca26674bbf702aacd7faa1b514216379f0695
Phoenix <phoenix.thorwall@gmail.com>
2024-12-12 15:38:01 -0500

    comments!

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,13 @@
+//Create function "Reveal()"
 Reveal = function(text){
+   //Create variable: In this context, stores the unordered list with the activity description within a parallel display
     var head = document.getElementById(text); 
+   //Create an if statement that displays hidden text on click
     if(head.style.display == 'none'){
        head.style.display = 'block';
-    } else {
+    } 
+    //Else statement that hides text on click if shown
+    else {
       head.style.display = 'none';
     }
  }
